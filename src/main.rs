285a00f69f46711e7d766b200e2a4a621fb38258
Gonçalo Valério <gon@ovalerio.net>
2020-09-27 00:25:10 +0100

    step two: add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{App, SubCommand};
+
 fn main() {
+    let matches = App::new("rgit vcs")
+        .version("0.1.0")
+        .author("Gonçalo Valério <gon@ovalerio.net>")
+        .about("A watered-down git clone")
+        .subcommand(SubCommand::with_name("init").about("creates new repository"))
+        .get_matches();
+
+    if let Some(_) = matches.subcommand_matches("init") {
+        init();
+    }
+}
+
+fn init() {
     println!("Hello, world!");
 }
