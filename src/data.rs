8a22a848619d4a5523ce8fad24b92622a74241fe
Gonçalo Valério <gon@ovalerio.net>
2020-09-29 00:04:28 +0100

    step four: Save object

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,26 @@
+use sha1::{Digest, Sha1};
 use std::fs;
+use std::str;
 
 static RGIT_DIR: &'static str = ".rgit";
 
 pub fn init() -> std::io::Result<()> {
     fs::create_dir(RGIT_DIR)?;
+    fs::create_dir(format!("{}/{}", RGIT_DIR, "objects"))?;
     Ok(())
+}
+
+pub fn hash_object(content: &Vec<u8>) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(content);
+    let digest = &hasher.finalize();
+    let s = format!("{:x}", digest);
+
+    fs::write(
+        format!("{}/{}/{}", RGIT_DIR, "objects", s),
+        content.as_slice(),
+    )
+    .expect("Failed to write object");
+
+    return s;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::{App, SubCommand};
+use clap::{App, Arg, ArgMatches, SubCommand};
+use std::fs;
 mod data;
 
 fn main() {
@@ -7,10 +8,17 @@ fn main() {
         .author("Gonçalo Valério <gon@ovalerio.net>")
         .about("A watered-down git clone")
         .subcommand(SubCommand::with_name("init").about("creates new repository"))
+        .subcommand(
+            SubCommand::with_name("hash-object")
+                .about("created an hash for an object")
+                .arg(Arg::with_name("file").index(1).required(true)),
+        )
         .get_matches();
 
-    if let Some(_) = matches.subcommand_matches("init") {
-        init();
+    match matches.subcommand_name() {
+        Some("init") => init(),
+        Some("hash-object") => hash_object(matches),
+        _ => println!("unknown sub command"),
     }
 }
 
@@ -18,5 +26,14 @@ fn init() {
     match data::init() {
         Ok(()) => println!("Repository created"),
         _ => println!("Failed. Perhaps the repository already exists."),
+    }
+}
+
+fn hash_object(matches: ArgMatches) {
+    if let Some(cmd_matches) = matches.subcommand_matches("hash-object") {
+        let content = fs::read(cmd_matches.value_of("file").unwrap())
+            .expect("Something went wrong reading the provided file");
+        let hash = data::hash_object(&content);
+        println!("{}", hash);
     }
 }
