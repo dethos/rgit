97f9c41bc95953cf489d00178b642886e919b771
Gonçalo Valério <gon@ovalerio.net>
2020-10-05 19:04:11 +0100

    step nine: Ignore .rgit files

--- src/base.rs
+++ src/base.rs
@@ -8,10 +8,23 @@ pub fn write_tree(directory: String) {
         let metadata = item.metadata().unwrap();
         let name = item.file_name();
         let full = format!("{}/{}", directory, name.to_str().unwrap());
+
+        if is_ignored(&full) {
+            continue;
+        }
+
         if metadata.is_file() {
             println!("{}", full);
         } else if metadata.is_dir() {
             write_tree(full);
         }
+    }
+}
+
+fn is_ignored(path: &String) -> bool {
+    if path.contains(".rgit") {
+        true
+    } else {
+        false
     }
 }
