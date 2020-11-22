459ded17090a366a7097271ff8ef31bbac43d794
Gonçalo Valério <gon@ovalerio.net>
2020-11-22 00:17:22 +0000

    step fourty four: print diff of commit

--- src/diff.rs
+++ src/diff.rs
@@ -1,4 +1,10 @@
 use std::collections::HashMap;
+use std::fs;
+use std::process::{Command, Stdio};
+use tempfile::NamedTempFile;
+
+#[path = "data.rs"]
+mod data;
 
 fn compare_trees(trees: Vec<HashMap<String, String>>) -> HashMap<String, Vec<String>> {
     let len_trees = trees.len();
@@ -18,6 +24,36 @@ fn compare_trees(trees: Vec<HashMap<String, String>>) -> HashMap<String, Vec<Str
     return entries;
 }
 
+fn diff_blobs(o_from: String, o_to: String, path: String) -> String {
+    let f_from = NamedTempFile::new().unwrap();
+    let f_to = NamedTempFile::new().unwrap();
+
+    if o_from != "" {
+        let content = data::get_object(o_from, "blob".to_owned());
+        fs::write(f_from.path(), content).unwrap();
+    }
+
+    if o_to != "" {
+        let content = data::get_object(o_to, "blob".to_owned());
+        fs::write(f_to.path(), content).unwrap();
+    }
+
+    let output = Command::new("diff")
+        .arg("--unified")
+        .arg("--show-c-function")
+        .arg("--label")
+        .arg(format!("a/{}", path))
+        .arg(f_from.path())
+        .arg("--label")
+        .arg(format!("a/{}", path))
+        .arg(f_to.path())
+        .stdout(Stdio::piped())
+        .output()
+        .expect("Failed to launch diff");
+
+    return String::from_utf8_lossy(&output.stdout).to_string();
+}
+
 pub fn diff_trees(t_from: HashMap<String, String>, t_to: HashMap<String, String>) -> String {
     let mut output = "".to_owned();
     let trees = vec![t_from, t_to];
@@ -25,7 +61,7 @@ pub fn diff_trees(t_from: HashMap<String, String>, t_to: HashMap<String, String>
         let o_from = oids[0].clone();
         let o_to = oids[1].clone();
         if o_from != o_to {
-            output.push_str(format!("changed {}\n", path).as_str());
+            output.push_str(diff_blobs(o_from, o_to, path.clone()).as_str());
         }
     }
 
